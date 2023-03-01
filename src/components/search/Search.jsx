7cd6dfa6b75c2d4cf4bcc75f73843858bd323a5a
samim-hossain-mondal <Samim_Hossain_Mondal@mckinsey.com>
2023-03-01 15:40:16 +0530

    feat: add search functionality

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -4,11 +4,18 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import './Search.css';
 
-export default function Search() {
+export default function Search({searchChangeHandler, data}) {
   const [search, setSearch] = useState('');
   const searchHandler = (e) => {
+    let searchInput = e.target.value;
     e.preventDefault();
     setSearch(e.target.value);
+    if(e.target.value === ''){
+      searchChangeHandler(data);
+      return;
+    }
+    let searchEvents = data.filter((event) => event.name.toLowerCase().includes(search.toLowerCase().trim()));
+    searchChangeHandler(searchEvents, searchInput);
   };
   console.log(search);
   return (
--- src/pages/eventList/EventList.jsx
+++ src/pages/eventList/EventList.jsx
@@ -1,18 +1,56 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { useNavigate } from 'react-router';
+import Card from '../../components/card/Card';
+import {BASE_URL, API_URL} from '../../constants/url';
 import Search from '../../components/search/Search';
 import Filter from '../../components/filter/Filter';
-import Body from '../../components/body/Body';
 import './EventList.css';
 
 export default function EventList() {
+  const [events, setEvents] = useState([]);
+  const [data, setData] = useState([]);
+  const fetchEvents = async () => {
+    const response = await fetch(`${BASE_URL}/${API_URL}`);
+    const data = await response.json();
+    setEvents(data);
+    setData(data);
+  };
+  useEffect(() => {
+    fetchEvents();
+  }, []);
+  console.log(events);
+  const navigate = useNavigate();
+  const clickHandler = (id) => {
+    navigate(`/event-details/${id}`);
+  };
+  const searchChangeHandler = (searchEvents) => {
+    setEvents(searchEvents);
+  };
   return (
     <div className='event-list'>
       <div className='event-list-header'>
         <Filter />
-        <Search />
+        <Search searchChangeHandler={searchChangeHandler} data={data}/>
       </div>
       <div className='event-list-body'>
-        <Body />
+        <div className="card-container">
+          {events.map((event) => (
+            <Card 
+              key={event.id}
+              id={event.id} 
+              name={event.name} 
+              description={event.description} 
+              venue={event.venue} 
+              datetime={event.datetime} 
+              timezone={event.datetime} 
+              areSeatsAvailable={event.areSeatsAvailable} 
+              isRegistered={event.isRegistered} 
+              isBookmarked={event.isBookmarked} 
+              imgUrl={event.imgUrl}
+              clickHandler={clickHandler}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
